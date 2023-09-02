ab8203bf2f9d0a70c6d07e5f4df72718d0c5aa51
Tejas <87278292+TejasK28@users.noreply.github.com>
2023-09-02 17:27:11 +0000

    C

--- C/pointers.c
+++ C/pointers.c
@@ -3,9 +3,13 @@
 int main()
 {
     int a = 5;
-    int * aptr = &a;
+    int * aptr = &a; // pts to the adress of a
 
     printf("a: %d\naptr: %p\n", a, aptr);
+
+
+    printf("%p\n", aptr); //  without *, you get address
+    printf("%d\n", *aptr); // with *, you get the value 
 
     return 0;
 }
